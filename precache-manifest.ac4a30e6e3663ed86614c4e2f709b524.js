20da60e71f80957130ce86c2e543a2f00847b622
Vikas KM <vikki.km@gmail.com>
2018-12-31 18:54:39 +0530

    Updates

---
+++ precache-manifest.ac4a30e6e3663ed86614c4e2f709b524.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b2558106d9bb3a7f9d3",
+    "url": "/React-Projects/person-list/static/css/main.aff6e080.chunk.css"
+  },
+  {
+    "revision": "3b2558106d9bb3a7f9d3",
+    "url": "/React-Projects/person-list/static/js/main.3b255810.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/React-Projects/person-list/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "7103dc0d3fa1d9828683",
+    "url": "/React-Projects/person-list/static/js/runtime~main.7103dc0d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Projects/person-list/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d26a458ca4ec50981190984f1b8cd90b",
+    "url": "/React-Projects/person-list/index.html"
+  }
+];
